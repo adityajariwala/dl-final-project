5b0232184ef13c95079c4deb9753d8d17ad0e287
Aditya Jariwala <ajariwal@utexas.edu>
2023-11-28 11:14:04 -0600

    add dist model fix

--- image_agent/models.py
+++ image_agent/models.py
@@ -157,7 +157,7 @@ def save_model(model, name: str = 'det.pt'):
 def load_model(name: str = 'model_puck.pt'):
     from torch import load
     from os import path
-    if name == 'model_puck.pt' or name == 'model_coord.pt':
+    if name == 'model_puck.pt' or name == 'model_coord.pt' or name == 'model_dist.pt':
         r = torch.load(path.join(path.dirname(path.abspath(__file__)), name))
     else:
         r = BinaryClassifier()
